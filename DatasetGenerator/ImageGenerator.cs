75859fe17b319889db0afd36268336daaa26709b
YeonGyu Jeong <81887821@hanmail.net>
2017-11-03 20:18:23 +0900

    Initial commit

---
+++ DatasetGenerator/ImageGenerator.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatasetGenerator
+{
+    class ImageGenerator
+    {
+        private const int IMAGE_WIDTH = 120;
+        private const int IMAGE_HEIGHT = 32;
+        private const int NUMBER_OF_IMAGES_TO_GENERATE = 10000;
+        private static readonly string[] BANNED_FONTS = { "HoloLens MDL2 Assets", "Marlett", "NewJumja", "Segoe MDL2 Assets", "Webdings", "Wingdings" };
+        private const bool DRAW_LINES = false;
+        private const int IMAGES_PER_THREAD = NUMBER_OF_IMAGES_TO_GENERATE / Program.NUMBER_OF_THREADS;
+
+        private readonly int part;
+        private Dictionary<int, int> values;
+
+        public ImageGenerator(int part)
+        {
+            this.part = part;
+            values = new Dictionary<int, int>();
+        }
+
+        public void GenerateImages()
+        {
+            FontFamily[] fonts = FontFamily.Families;
+            Random random = new Random();
+
+            for (int i = part * NUMBER_OF_IMAGES_TO_GENERATE / Program.NUMBER_OF_THREADS; i < (part + 1) * NUMBER_OF_IMAGES_TO_GENERATE / Program.NUMBER_OF_THREADS; i++)
+            {
+                int digit = random.Next() % 6 + 1;
+                int number = random.Next() % ((int)Math.Pow(10, digit));
+                Bitmap bitmap = new Bitmap(IMAGE_WIDTH, IMAGE_HEIGHT);
+                int fontIndex = random.Next() % fonts.Length;
+
+                while (BANNED_FONTS.Contains(fonts[fontIndex].Name))
+                    fontIndex = random.Next() % fonts.Length;
+
+                using (Graphics graphics = Graphics.FromImage(bitmap))
+                {
+                    using (Font font = new Font(fonts[fontIndex].Name, 15))
+                    {
+                        graphics.FillRectangle(Brushes.White, new Rectangle(0, 0, IMAGE_WIDTH, IMAGE_HEIGHT));
+                        graphics.DrawString(number.ToString(), font, Brushes.Black, new PointF(10f, 5f));
+                        int lines = random.Next() % 5 + 3;
+                        if (DRAW_LINES)
+                        {
+                            for (int j = 0; j < lines; j++)
+                                graphics.DrawLine(new Pen(Brushes.Black, 1f), new Point(random.Next() % IMAGE_WIDTH, random.Next() % IMAGE_HEIGHT), new Point(random.Next() % IMAGE_WIDTH, random.Next() % IMAGE_HEIGHT));
+                        }
+                    }
+                }
+                bitmap.Save(Program.DIRECTORY_TO_SAVE + i + Program.IMAGE_EXTTENSION);
+                values.Add(i, number);
+
+                if (part == 0 && i % 64 == 0)
+                {
+                    Console.WriteLine((i + 1) + " / " + IMAGES_PER_THREAD);
+                }
+            }
+        }
+
+        public Dictionary<int, int> GetValues()
+        {
+            return values;
+        }
+    }
+}
---
+++ DatasetGenerator/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace DatasetGenerator
+{
+    class Program
+    {
+        public const int NUMBER_OF_THREADS = 4;
+        public const string DIRECTORY_TO_SAVE = @"S:\";
+        public const string IMAGE_EXTTENSION = ".jpg";
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Thread[] threads = new Thread[NUMBER_OF_THREADS];
+            ImageGenerator[] imageGenerators = new ImageGenerator[NUMBER_OF_THREADS];
+
+            for (int i = 0; i < NUMBER_OF_THREADS; i++)
+            {
+                imageGenerators[i] = new ImageGenerator(i);
+                threads[i] = new Thread(imageGenerators[i].GenerateImages);
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < NUMBER_OF_THREADS; i++)
+            {
+                threads[i].Join();
+            }
+
+            using (StreamWriter writer = new StreamWriter(DIRECTORY_TO_SAVE + "label.txt"))
+            {
+                for (int i = 0; i < NUMBER_OF_THREADS; i++)
+                {
+                    Dictionary<int, int> values = imageGenerators[i].GetValues();
+                    foreach (KeyValuePair<int, int> value in values)
+                    {
+                        writer.WriteLine(value.Key + IMAGE_EXTTENSION + " " + value.Value);
+                    }
+                }
+            }
+        }
+    }
+}
